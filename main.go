a671fea6dba4d47170b28c6d031551e53a40da33
VsalCode <vsal.ilham@gmail.com>
2025-07-01 16:52:28 +0700

    chore: implement database connection utility and update dependencies

--- main.go
+++ main.go
@@ -1,3 +1,16 @@
-package main;
+package main
 
-func main(){}
+import (
+	"be-cinevo/utils"
+	"fmt"
+) 
+
+func main() {
+	db, err := utils.DBConnect()
+
+	if err != nil {
+		fmt.Println("Failed to connect to database:", err)
+	}
+	defer db.Close()
+
+}
---
+++ utils/db.go
@@ -0,0 +1,32 @@
+package utils
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/jackc/pgx/v5/pgxpool"
+	"github.com/joho/godotenv"
+)
+
+func DBConnect() (*pgxpool.Pool, error) {
+	godotenv.Load()
+
+	connectionString := fmt.Sprintf(
+		"postgres://%s:%s@%s:%s/%s?sslmode=disable",
+		os.Getenv("PGUSER"),
+		os.Getenv("PGPASSWORD"),
+		os.Getenv("PGHOST"),
+		os.Getenv("PGPORT"),
+		os.Getenv("PGDATABASE"),
+	)
+
+	pool, err := pgxpool.New(context.Background(), connectionString)
+	if err != nil {
+		return nil, err
+	}
+
+	log.Println("✅ Database connection successful!")
+	return pool, nil
+}
